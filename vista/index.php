f382d0da06a1e73f1bd81619152591c06e2e1f11
Carlos Sepulveda <carlos.sepulvedahe@gmail.com>
2020-05-12 01:57:01 -0400

    Diseño del login

---
+++ vista/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/estiloIndex.css">
+    <link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header><!--Logo y boton de inicio de sesion-->
+        <img src="./img/logo.png" alt="logo">
+
+        <input class="button_mediano" type="submit" value="Iniciar Sesion">
+        
+    </header>
+
+    <section class="cuerpo">
+
+        <img id="imagendeco" src="./img/fondo.jpg" alt="">
+
+        <section>
+        <p>Diseñemos algo increible!</p>
+
+        <form action="" method="post">
+
+            <input type="text" placeholder="Nombre">
+		    <input type="text" placeholder="Correo Electronico">
+            <input type="text" placeholder="Contraseña">
+            <input type="text" placeholder="Confirmar contraseña">
+            <section class="loginextra"> 
+                <section>
+                <img class="logincon" src="./img/fbdark.png" alt="">
+                <img class="logincon" src="./img/google.png" alt="">
+                </section>
+                <input  class="button_grande" type="submit" value="GO!">
+            </section>
+            
+
+        </form>
+        </section>
+
+    </section>
+
+
+
+    <footer>
+
+        <section>
+            <p>Tus diseños siempre contigo.</p>
+            <p>Boceto © 2020</p>
+        </section>
+
+        <section>
+            <img class="icon" id="fb" src="./img/fb.png" alt="">
+            <img class="icon" id="instagram" src="./img/instagram.png" alt="">
+            <img class="icon" id="pinterest" src="./img/pinterest.png" alt="">
+        </section>
+
+    </footer>
+
+
+</body>
+</html>
